68d2e9771caa010ac8d06cacdb609efa2de479c2
Julik Tarkhanov <me@julik.nl>
2017-02-24 01:53:38 +0100

    Implement altitude-dependend magvar for points

--- lib/points.rb
+++ lib/points.rb
@@ -1,10 +1,14 @@
 module PointInfo
+  def elev_km
+    10.0 # 10000 mtr above MSL is ~ standard cruise altitude for a Tu-154
+  end
+  
   def radio?
     false
   end
   
   def magnetic_variation
-    Magvar.variation_at(lat, lon, elev_km=0)
+    Magvar.variation_at(lat, lon, elev_km, Time.utc(2020,1,1))
   end
 end
 
@@ -22,7 +26,15 @@ end
 
 class NDB   < Struct.new(:lat, :lon, :ident, :name, :elev_ft, :freq_int, :reception_range_nm)
   include PointInfo
-  def radio?; true; end
+  def elev_km
+    ft_to_km = 0.0003048
+    elev_ft * ft_to_km
+  end
+  
+  def radio?
+    true
+  end
+
   def to_s
     "NDB %s (%s %s)\n%s" % [freq_int, ident, Morse.code(ident), name]
   end
@@ -30,15 +42,33 @@ end
 
 class VOR   < Struct.new(:lat, :lon, :ident, :name, :elev_ft, :freq_int, :reception_range_nm)
   include PointInfo
-  def radio?; true; end
+
+  def elev_km
+    ft_to_km = 0.0003048
+    elev_ft * ft_to_km
+  end
+  
+  def radio?
+    true
+  end
+
   def to_s
     "VOR %s (%s %s)\n%s" % [freq_int, ident, Morse.code(ident), name]
   end
 end
 
-class DME   < Struct.new(:lat, :lon, :ident, :name, :elev_ft, :freq_int, :nm_dme_bias)
+class DME < Struct.new(:lat, :lon, :ident, :name, :elev_ft, :freq_int, :nm_dme_bias)
   include PointInfo
-  def radio?; true; end
+
+  def elev_km
+    ft_to_km = 0.0003048
+    elev_ft * ft_to_km
+  end
+  
+  def radio?
+    true
+  end
+
   def to_s
     "DME %s (%s %s)\n%s" % [freq_int, ident, Morse.code(ident), name]
   end
@@ -46,7 +76,15 @@ end
 
 class RSBN   < Struct.new(:lat, :lon, :ident, :name, :chan, :elev_mtr)
   include PointInfo
-  def radio?; true; end
+
+  def elev_km
+    elev_mtr / 1000.0
+  end
+  
+  def radio?
+    true
+  end
+  
   def to_s
     "%s (%s %s)\n%s" % [chan, ident, Morse.code(ident), name]
   end
