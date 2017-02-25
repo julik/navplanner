739ea07256b3917852b38d5202fca822f60db94e
Julik Tarkhanov <me@julik.nl>
2017-02-25 18:33:20 +0100

    Rudimentary error handling

--- lib/navplanner_web.rb
+++ lib/navplanner_web.rb
@@ -4,7 +4,8 @@ require 'thread'
 
 class NavplannerWeb < Sinatra::Base
   set :views, File.join(__dir__, "web_app_views")
-
+  IncorrectPlan = Class.new(StandardError)
+  
   before do
     lang_code = params[:lang] || :en
     $stderr.puts "Setting locale %s" % lang_code
@@ -20,31 +21,35 @@ class NavplannerWeb < Sinatra::Base
     wpt_list = wpt_list.strip.split(/\s+/).map(&:upcase)
     @parsed_wpt_list = wpt_list.join(' ')
     
-    raise "Need at least 2 waypoints" if wpt_list.length < 2
-    
-    db = load_nav_database
-    
-    # Parse legs from the waypoint list
-    legs = Planner.wyaypoint_list_to_legs(db, wpt_list)
-    
-    # Print a normal great-circle plan
-    @result_fpl = buffer{|b| PlanPrinter.print_plan(legs, b) }
-    
-    # Print a palette report for NVU use
-    beacon_list = db.of_class(RSBN)
-    @result_nvu = buffer{|b| NVUPrinter.new.print_plan(legs, beacon_list, b) }
+    begin
+      raise IncorrectPlan, "Need at least 2 waypoints" if wpt_list.length < 2
+      db = load_nav_database
 
-    # Print a plan for NAS-1
-    @result_nas = buffer{|b| NAS1Printer.new.print_plan(legs, b) }
+      # Parse legs from the waypoint list. In practice this is the only
+      # phase that can lead to an error.
+      legs = Planner.wyaypoint_list_to_legs(db, wpt_list)
+    
+      # Print a normal great-circle plan
+      @result_fpl = buffer{|b| PlanPrinter.print_plan(legs, b) }
+    
+      # Print a palette report for NVU use
+      beacon_list = db.of_class(RSBN)
+      @result_nvu = buffer{|b| NVUPrinter.new.print_plan(legs, beacon_list, b) }
 
-    # Print a plan that can be loaded into the KLN
-    @result_kln = buffer{|b| XPFMSPrinter.print_plan(legs, b) }
+      # Print a plan for NAS-1
+      @result_nas = buffer{|b| NAS1Printer.new.print_plan(legs, b) }
+
+      # Print a plan that can be loaded into the KLN
+      @result_kln = buffer{|b| XPFMSPrinter.print_plan(legs, b) }
+    rescue => planning_error
+      @error = planning_error
+    end
     
     haml :index
   end
-  
+
   DB_CACHE_MUX = Mutex.new
-  
+
   def load_nav_database
     DB_CACHE_MUX.synchronize do
       @@cached_db ||= begin
