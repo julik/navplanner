3d947808de8c8f894aad8a45b76a9747d1336e65
Julik Tarkhanov <me@julik.nl>
2017-03-05 16:22:27 +0100

    Print magnetic variation as +- decimal too

--- lib/nas1_printer.rb
+++ lib/nas1_printer.rb
@@ -39,7 +39,7 @@ class NAS1Printer
         radio = to.radio? ? to.to_s : '-'
         row << radio
 
-        row << "%s / %s" % [decimal_degrees(from.magnetic_variation), decimal_degrees(to.magnetic_variation)]
+        row << "%0.1f / %0.1f" % [from.magnetic_variation, to.magnetic_variation]
 
         t << row
       end
